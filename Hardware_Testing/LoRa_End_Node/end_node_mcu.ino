5b67763e6d140a9699822663dea95ccdbb3c8b9c
NamalJayasuriya <namalcreation@gmail.com>
2017-07-17 23:34:13 +0530

    msg format is updated

--- Hardware_Testing/LoRa_End_Node/end_node_mcu.ino
+++ Hardware_Testing/LoRa_End_Node/end_node_mcu.ino
@@ -24,28 +24,22 @@ TinyGPSPlus gps;
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
 
-void setup()
-{
+void setup(){
   pinMode(3,INPUT); //gpio pin for AUX
   pinMode(4,OUTPUT);//gpio pin for M1
   pinMode(5,OUTPUT);//gpio pin for M2
   digitalWrite(3,HIGH);//initialize AUX
   digitalWrite(4,LOW);//initialize M1
   digitalWrite(5,LOW);//initialize m2
+  
   Serial.begin(9600);
-  Serial1.begin(9600);
-
-  
   ss.begin(GPSBaud);
-
-  Serial.println(F("LoRa end Node demonstration"));
-  
+  //Serial.println(F("LoRa end Node demonstration"));
 }
 
-void loop()
-{
+void loop(){
   int a=digitalRead(3);
-  
+    
   while (ss.available() > 0 && a==1){
     delay(2);
     if (gps.encode(ss.read())){
@@ -54,61 +48,72 @@ void loop()
     }
   }
   
-  if (millis() > 5000 && gps.charsProcessed() < 10)
-  {
+  if (millis() > 5000 && gps.charsProcessed() < 10){
     Serial.println(F("No GPS detected: check wiring."));
-    while(true);
+    while(true)
+    delay(1000);
   }
   
 }
-
-void displayInfo()
-{
-  Serial1.print(F("Loc: ")); 
-  if (gps.location.isValid())
-  {
-    Serial1.print(gps.location.lat(), 6);
-    Serial1.print(F(","));
-    Serial1.print(gps.location.lng(), 6);
+    
+void displayInfo(){
+  String disp;
+  if (gps.location.isValid()){
+    disp=disp+"L:";
+    if (gps.location.isValid()){
+      if (int(gps.location.lat())< 10) disp=disp+"0";
+      disp=disp+String(gps.location.lat(),6);
+      disp=disp+",";
+      if (int(gps.location.lng())< 10) disp=disp+"0";
+      disp=disp+String(gps.location.lng(), 6);
+    }
+    
+    else{
+      //Serial.print(F("INVALID"));
+    }
+  
+    disp=disp+" D:";
+    if (gps.date.isValid()){
+      if (gps.date.month()< 10) disp=disp+"0";
+      disp=disp+gps.date.month();
+      disp=disp+"/";
+      if (gps.date.day()< 10) disp=disp+"0";
+      disp=disp+gps.date.day();
+      
+    }
+    else{
+      //Serial.print(F("INVALID"));
+    }
+  
+    if (gps.time.isValid()){
+      int hrs=gps.time.hour()+5;
+      int mns=gps.time.minute()+30;
+      hrs=hrs+mns/60;
+      mns=mns%60;
+      
+      disp=disp+" T:";
+      if (hrs< 10) disp=disp+"0";
+      disp=disp+hrs;
+      disp=disp+":";
+      if ( mns< 10) disp=disp+"0";
+      disp=disp+mns;
+      disp=disp+":";
+      if (gps.time.second() < 10) disp=disp+"0";
+      disp=disp+gps.time.second();
+      //Serial.print(F("."));
+      //if (gps.time.centisecond() < 10) Serial.print(F("0"));
+      //Serial.print(gps.time.centisecond());
+      
+    }
+    else{
+      //Serial.print(F("INVALID"));
+    }
+    Serial.println(disp);
+    }
+  
+  else{
+    Serial.println(F("PGS Not Fixed"));
+    delay(1000);
   }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial1.print(F("  D/T: "));
-  if (gps.date.isValid())
-  {
-    Serial1.print(gps.date.month());
-    Serial1.print(F("/"));
-    Serial1.print(gps.date.day());
-    Serial1.print(F("/"));
-    Serial1.print(gps.date.year());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
-    Serial1.print(gps.time.hour());
-    Serial1.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial1.print(gps.time.minute());
-    Serial1.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial1.print(gps.time.second());
-    Serial1.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial1.print(gps.time.centisecond());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial1.println();
+  
 }
