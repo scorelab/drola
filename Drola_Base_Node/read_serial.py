3659793a68763c860e97b9d79e9ce9a1e9aefd89
NamalJayasuriya <namalcreation@gmail.com>
2017-07-21 15:40:13 +0530

    Implemented AES security end node to base node communication

---
+++ Drola_Base_Node/read_serial.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+
+#BS = 16
+#pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+#unpad = lambda s : s[:-ord(s[len(s)-1:])]
+
+import base64
+from Crypto.Cipher import AES
+from Crypto import Random
+
+import serial, time
+ser = serial.Serial('/dev/ttyUSB0',9600)
+
+
+class AESCipher:
+    def __init__( self, key ,iv):
+        self.key = key
+        self.iv=iv
+
+    def encrypt( self, raw ):
+        #raw = pad(raw)
+        cipher = AES.new( self.key, AES.MODE_CBC, self.iv )
+        #return base64.b64encode( iv + cipher.encrypt( raw ) )
+        return ( self.iv + cipher.encrypt( raw ) )
+
+    def decrypt( self, enc ):
+        #enc = base64.b64decode(enc)
+        #iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv )
+
+        return cipher.decrypt( enc )
+
+
+key=bytearray([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31])
+iv = bytearray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+
+aes = AESCipher(str(key),str(iv))
+
+#----------test AES------------------------
+# text="abcdefghijklmnopqrstuvwxyz12345"
+# # print "text len :", len(text)
+# cypher = aes.encrypt(text)
+# print "cypertext: ",cypher, len(cypher)
+#
+# plain = aes.decrypt(cypher)
+# print "\nthis :", plain
+
+#----------------------------------------------
+
+while 1:
+    data=ser.readline()
+    print data,len(data)
+    if len(data) > 30:
+        plain = aes.decrypt(data[:32])
+        print "decrypted :", plain
+    else:
+        continue
+    #ser.flushInput()
+    time.sleep(1)
--- LoRa_End_Node/end_node_mcu.ino
+++ LoRa_End_Node/end_node_mcu.ino
@@ -3,7 +3,7 @@
 HardWare Used
 
 --------*******************-------------
-ATMega 1280
+ATMega 328P
 LoRa E45-TTL-100
 GY-GPS-6MV2 
 --------*******************-------------
@@ -14,9 +14,14 @@ collects gps data from jps module throught TinyGPSPLus and Software serial Libra
 
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+#include <AESLib.h>
 
 static const int RXPin = 10, TXPin = 11;
 static const uint32_t GPSBaud = 9600;
+
+uint8_t key[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+uint8_t iv[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
 
 // The TinyGPS++ object
 TinyGPSPlus gps;
@@ -31,7 +36,7 @@ void setup(){
   digitalWrite(3,HIGH);//initialize AUX
   digitalWrite(4,LOW);//initialize M1
   digitalWrite(5,LOW);//initialize m2
-  
+
   Serial.begin(9600);
   ss.begin(GPSBaud);
   //Serial.println(F("LoRa end Node demonstration"));
@@ -39,27 +44,50 @@ void setup(){
 
 void loop(){
   int a=digitalRead(3);
-    
-  while (ss.available() > 0 && a==1){
-    delay(2);
-    if (gps.encode(ss.read())){
-      displayInfo();
-      delay(10);
-    }
-  }
-  
-  if (millis() > 5000 && gps.charsProcessed() < 10){
-    Serial.println(F("No GPS detected: check wiring."));
-    while(true)
-    delay(1000);
-  }
-  
+  char data[] = "L06.870808,79.880714,0718,184455";// L<latitude>,<longitude>,<date>,<time>
+  aes_context ctx;
+            ctx = aes256_cbc_enc_start(key, iv);
+            aes_cbc_enc_continue(ctx, data, 32);
+            aes_cbc_enc_finish(ctx);
+            Serial.println(data);
+            delay(1000);
+//  while (ss.available() > 0 && a==1){
+//    Serial.println("gps is available");
+//    delay(2);
+//    if (gps.encode(ss.read())){
+//      if (gps.location.isValid()){
+//            //String data1=getGPSData();
+//            aes_context ctx;
+//            ctx = aes256_cbc_enc_start(key, iv);
+//            aes_cbc_enc_continue(ctx, data, 32);
+//            aes_cbc_enc_finish(ctx);
+//            Serial.println(data);
+//            delay(1000);
+//        }else{
+//            return(F("PGS Not Fixed"));
+//            delay(1000);
+//        }
+//      delay(10);
+//    }
+//    else{
+//      Serial.println("Error , cannot read gps");
+//      delay(1000);
+//      }
+//  }
+//
+//  if (millis() > 5000 && gps.charsProcessed() < 10){
+//    Serial.println(F("No GPS detected: check wiring."));
+//    //while(true)
+//    delay(1000);
+//  }
+
 }
-    
-void displayInfo(){
-  String disp;
-  if (gps.location.isValid()){
-    disp=disp+"L:";
+
+
+String getGPSData(){
+  String disp="";
+
+    //disp=disp+"L:";
     if (gps.location.isValid()){
       if (int(gps.location.lat())< 10) disp=disp+"0";
       disp=disp+String(gps.location.lat(),6);
@@ -67,53 +95,50 @@ void displayInfo(){
       if (int(gps.location.lng())< 10) disp=disp+"0";
       disp=disp+String(gps.location.lng(), 6);
     }
-    
+
     else{
       //Serial.print(F("INVALID"));
     }
-  
-    disp=disp+" D:";
+
+    disp=disp+",";//" D:";
     if (gps.date.isValid()){
       if (gps.date.month()< 10) disp=disp+"0";
       disp=disp+gps.date.month();
-      disp=disp+"/";
+      //disp=disp+"/";
       if (gps.date.day()< 10) disp=disp+"0";
       disp=disp+gps.date.day();
-      
+
     }
     else{
       //Serial.print(F("INVALID"));
     }
-  
+
     if (gps.time.isValid()){
       int hrs=gps.time.hour()+5;
       int mns=gps.time.minute()+30;
       hrs=hrs+mns/60;
       mns=mns%60;
-      
-      disp=disp+" T:";
+
+      disp=disp+",";//" T:";
       if (hrs< 10) disp=disp+"0";
       disp=disp+hrs;
-      disp=disp+":";
+      //disp=disp+":";
       if ( mns< 10) disp=disp+"0";
       disp=disp+mns;
-      disp=disp+":";
+      //disp=disp+":";
       if (gps.time.second() < 10) disp=disp+"0";
       disp=disp+gps.time.second();
       //Serial.print(F("."));
       //if (gps.time.centisecond() < 10) Serial.print(F("0"));
       //Serial.print(gps.time.centisecond());
-      
+
     }
     else{
       //Serial.print(F("INVALID"));
     }
-    Serial.println(disp);
+    //Serial.println(disp);
+    return (disp);
     }
-  
-  else{
-    Serial.println(F("PGS Not Fixed"));
-    delay(1000);
-  }
-  
-}
+
+
+
