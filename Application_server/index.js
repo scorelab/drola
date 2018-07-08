51df9306957db6d1fef82b8ec63266d08d3dd696
janagan1993 <janagan1993@gmail.com>
2018-07-08 13:23:56 +0530

    drola web client standerdized accordong to the first evaluation comments.

--- Application_server/index.js
+++ Application_server/index.js
@@ -1,72 +1,71 @@
+import express, { Router } from 'express';
+import cookieParser from 'cookie-parser';
+import { json } from 'body-parser';
+import { MongoClient } from 'mongodb';
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 
-var express = require("express");
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// -----------------express app------------------------------------
+app.use(cookieParser());
+app.use(json());
+const router = Router();
 
-//----------------------mongo changeStream---------------------------------------
-//pipeline for watching mongo change stream
+app.use('/', router);
+
+app.use(express.static('web'));
+
+
+// ----------------------mongo changeStream---------------------------------------
+// pipeline for watching mongo change stream
 const pipeline = [
   {
     $match: {
-      'fullDocument.node': {$exists:true, $gt : ""},
-      $and: [{operationType: "insert" }]
-    }
+      'fullDocument.node': { $exists: true, $gt: '' },
+      $and: [{ operationType: 'insert' }],
+    },
   },
 ];
 
 //create a MongoClient connection
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/drola?replicaSet=rs0";
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
+
+const url = "mongodb://localhost:27017/drola?replicaSet=rs0";
+MongoClient.connect(url,{ useNewUrlParser: true }, (err, db) =>{
+  if (err) {
+    console.log(err)
+    throw err
+  };
   var dbo = db.db("drola");
   const collection = dbo.collection("nodes");
   let changeStream = collection.watch(pipeline);
 
   // start listen to changes
-  changeStream.on("change", function(change) {
+  changeStream.on("change", (change)=> {
         //console.log(change.fullDocument);
         //forward the notified changes to client via socket
         socket.emit('data', change.fullDocument);
     });
 });
 //---------------------------------------------------------------
-//-----------------express app------------------------------------
-app.use(cookieParser());
-app.use(bodyParser.json());
-var router = express.Router();
-var path = __dirname + '/';
 
-router.use(function (req,res,next) {
-    next();
-});
 
-router.get("/",function(req,res){
-    res.sendFile(path + "web/index.html");
-});
-
-app.use("/",router);
-app.use("/css",express.static('web/css'));
-app.use("/js",express.static('web/js'));
-app.use("/images",express.static('web/images'));
-app.use("/fonts",express.static('web/fonts'));
-
-//------------------socket connection------------------------
-//create a socket connection using socket.io
-socket= io.on('connection', function (socket) {
+// ------------------socket connection------------------------
+// create a socket connection using socket.io
+const Socket = io.on('connection', (socket) => {
   // socket.emit('data', { hello: 'world' });
-  //log to the console when acknoledgement received from the client
-  socket.on('ack', function (data) {
+  // log to the console when acknowledgement received from the client
+  socket.on('ack', (data) => {
     console.log(data);
-    });
-  return socket;
   });
+  return socket;
+});
+
+
 //-----------------------------------------------------------
-server.listen(3000,function(){
-    console.log("Communicaton link, Live at Port 3000");
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Communication link, Live at Port ${port}`);
 });
 //--------------------------------------------------------
