b9d688484a8b83f79d5de180a81c5f2b46aa521b
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2018-11-06 17:48:12 +0100

    Use ES6 code

--- Drola_Base_Node/index.js
+++ Drola_Base_Node/index.js
@@ -1,59 +1,57 @@
-var express = require("express");
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-//var re=require("socket");
-var CronJob = require('cron').CronJob;
+const express = require("express");
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+//const re=require("socket");
+const CronJob = require('cron').CronJob;
 
-var app = express();
+const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-var router = express.Router();
-var path = __dirname + '/';
+const router = express.Router();
+const path = __dirname + '/';
 
-router.use(function (req,res,next) {
-    next();
-});
+router.use((req, res, next) => next());
 
-router.get("/",function(req,res){
+router.get("/", (req, res) => {
     //checkAndredirect(req,res);
     res.sendFile(path + "web/index.html");
 });
 
-app.use("/",router);
-app.use("/css",express.static('web/css'));
-app.use("/js",express.static('web/js'));
-app.use("/images",express.static('web/images'));
-app.use("/fonts",express.static('web/fonts'));
+app.use("/", router);
+app.use("/css", express.static('web/css'));
+app.use("/js", express.static('web/js'));
+app.use("/images", express.static('web/images'));
+app.use("/fonts", express.static('web/fonts'));
 
-
-app.listen(3000,function(){
-    console.log("Communicaton link, Live at Port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Communicaton link, Live at Port ${port}`);
 });
 
 ///================Web socket server ======================//
 /*
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+const WebSocketServer = require('websocket').server;
+const http = require('http');
 
-var server = http.createServer(function(request, response) {
+const server = http.createServer((request, response) => {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
-server.listen(8080, function() {
+server.listen(8080, () => {
     console.log((new Date()) + 'Data Socket is listening on port 8080');
 });
 
-var wsServer = new WebSocketServer({
+const wsServer = new WebSocketServer({
     httpServer: server,
 
     autoAcceptConnections: false
 });
 
-var listners=[];
+const listners=[];
 function notifyListners(){
-    for (var j=0;j=listners.length;j++){
+    for (const j=0;j=listners.length;j++){
         if(listners[j]!=undefined) {
             listners[j].sendUTF(JSON.stringify([{lat: 8.742579, lng: 80.288086, name: "dr1"}, {
                 lat: 8.118373,
@@ -65,16 +63,16 @@ function notifyListners(){
 
 }
 
-wsServer.on('request', function(request) {
+wsServer.on('request', (request) => {
 
-    var connection = request.accept('dronelink', request.origin);
+    const connection = request.accept('dronelink', request.origin);
     console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
+    connection.on('message', (message) => {
         if (message.type === 'utf8') {
             console.log('Received Message: ' + message.utf8Data);
             listners.push(connection);
             console.log(listners);
-            new CronJob('* * * * * *', function() {
+            new CronJob('* * * * * *', () => {
                 connection.sendUTF(JSON.stringify([{lat:8+Math.random(),lng:80+Math.random(),name:"dr1"},{lat:8+Math.random(),lng:80+Math.random(),name:"dr2"},{lat:8+Math.random(),lng:80+Math.random(),name:"dr3"}]));
             }, null, true, 'America/Los_Angeles');
 
@@ -84,7 +82,7 @@ wsServer.on('request', function(request) {
             connection.sendBytes(message.binaryData);
         }
     });
-    connection.on('close', function(reasonCode, description) {
+    connection.on('close', (reasonCode, description) => {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
